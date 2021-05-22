cbd3d5dc388ba0f547c5edd96987c7f6f474b0f5
dural <dural@snapchat.com>
2021-05-22 18:51:00 +0200

    initial commit with basic instructions

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # avax-updater
+
+Replace your username with the `USER` text and add this line under your `/etc/sudoers` file.
+
+```
+USER ALL = (root) NOPASSWD: /home/USER/avax-updater/avax-updater.sh
---
+++ avax-updater.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash
+
+AVALANCHE_FOLDER="$GOPATH/src/github.com/ava-labs/gecko"
+
+cd "$AVALANCHE_FOLDER" || exit 1
+
+git fetch origin
+reslog=$(git log HEAD..origin/master --oneline)
+if [[ "${reslog}" != "" ]] || [[ $1 = "force" ]] ; then
+  git merge origin/master # completing the pull
+  if [ $? -eq 0 ]; then
+    bash -c "$AVALANCHE_FOLDER/scripts/build.sh"
+
+    if [ $? -eq 0 ]; then
+        sudo systemctl restart avalanche.service
+    else
+        echo "scipts/build.sh returned a non-zero code. Exiting..."
+        exit 1
+    fi
+
+  else
+    echo "git pull failed. Exiting..."
+    exit 1
+  fi
+else
+    echo "Already up to date"
+    exit 0
